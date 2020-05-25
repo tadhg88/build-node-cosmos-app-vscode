954db8f7b5ba0c2b20ee6b2fb028b7c9d0692964
tadhg88 <tadhg88@gmail.com>
2020-05-25 21:25:27 +0100

    adding package and helper methods to query, insert, update, and delete documents in a Cosmos DB database.

--- index.js
+++ index.js
@@ -1,6 +1,90 @@
 //@ts-check
 var question = require("readline-sync").question;
 
+var config = {
+  connectionString:
+    "AccountEndpoint=https://tadhg88-cosmosdb-test.documents.azure.com:443/;AccountKey=Samw4seqXpOe6MFy64gvAwB9u5Y0u1a8kFb864XLAxfQk6ldHWd0YfvvxjH7dp7V9vaMLPH1PyJ6j4lLV4H4Og==",
+  database: "SchoolDB",
+  container: "StudentCourseGrades",
+};
+
+var cosmos = require("@azure/cosmos");
+
+const client = new cosmos.CosmosClient(config.connectionString);
+const databaseid = config.database;
+const containerid = config.container;
+const containerref = client.database(databaseid).container(containerid);
+const containerdata = containerref.items;
+
+function isOK(statusCode) {
+  return statusCode >= 200 && statusCode <= 299;
+}
+
+async function addStudent(student) {
+  const { item, statusCode } = await containerdata.create(student).catch();
+  isOK(statusCode) &&
+    process.stdout.write(`Added student with id: ${item.id}\n`);
+}
+
+async function updateStudent(student) {
+  const { item, statusCode } = await containerdata.upsert(student).catch();
+  isOK(statusCode) &&
+    process.stdout.write(`Updated student with id: ${item.id}\n`);
+}
+
+async function deleteStudent(student) {
+  const { item, statusCode } = await containerref
+    .item(student.id, student.StudentNumber)
+    .delete()
+    .catch();
+  isOK(statusCode) &&
+    process.stdout.write(`Deleted student with id: ${item.id}\n`);
+}
+
+// This function fetches a student document, given the id and the studentNumber (the partition key)
+async function getStudent(ID, studentNumber) {
+  const { resource, statusCode } = await containerref
+    .item(ID, studentNumber)
+    .read()
+    .catch();
+  if (isOK(statusCode)) {
+    process.stdout.write(
+      `Student data: ${resource.StudentNumber}: ${resource.Forename}, ${resource.Lastname}\n`
+    );
+    resource.CourseGrades.forEach(function (coursegrade) {
+      process.stdout.write(`${coursegrade.Course}:${coursegrade.Grade}\n`);
+    });
+    return new Student(
+      resource.id,
+      resource.StudentNumber,
+      resource.Forename,
+      resource.Lastname
+    );
+  }
+  return null;
+}
+
+async function queryStudents(courseName) {
+  const studentquery = {
+    query:
+      "SELECT s.StudentNumber, s.Forename, s.Lastname, c.Course, c.Grade \
+                FROM students s JOIN c IN s.CourseGrades \
+                WHERE c.Course = @coursename",
+    parameters: [
+      {
+        name: "@coursename",
+        value: courseName,
+      },
+    ],
+  };
+
+  const { resources } = await containerdata.query(studentquery).fetchAll();
+  for (let queryResult of resources) {
+    let resultString = JSON.stringify(queryResult);
+    process.stdout.write(`\nQuery returned ${resultString}\n`);
+  }
+}
+
 class Student {
   constructor(ID, studentNumber, forename, lastname) {
     this.id = ID;
@@ -33,19 +117,72 @@ function getStudentData() {
   return student;
 }
 
-function test() {
+async function test() {
+  process.stdout.write("\n\nTesting addStudent and getStudent\n\n");
+
+  // Create a new student
   let student1 = getStudentData();
-  student1.addGrade("Computer Science", "A");
-  student1.addGrade("Applied Mathematics", "C");
+  await addStudent(student1).then(() =>
+    getStudent(student1.id, student1.StudentNumber)
+  );
 
-  process.stdout.write(student1.toString());
-  process.stdout.write(student1.getGrades());
+  process.stdout.write("\n\n");
 
+  // Create another student
   let student2 = getStudentData();
-  student2.addGrade("Computer Science", "A");
+  await addStudent(student2).then(() =>
+    getStudent(student2.id, student2.StudentNumber)
+  );
 
-  process.stdout.write(student2.toString());
-  process.stdout.write(student2.getGrades());
+  process.stdout.write("\n\n");
+
+  // The first student got an A in Physics and a C in Chemistry
+  process.stdout.write("\n\nTesting updateStudent\n\n");
+  student1.addGrade("Physics", "A");
+  student1.addGrade("Chemistry", "C");
+  await updateStudent(student1).then(() =>
+    getStudent(student1.id, student1.StudentNumber)
+  );
+
+  process.stdout.write("\n");
+
+  // The second student got a B in Physics and a D in Mathematics
+  student2.addGrade("Physics", "B");
+  student2.addGrade("Mathematics", "D");
+  await updateStudent(student2).then(() =>
+    getStudent(student2.id, student2.StudentNumber)
+  );
+
+  process.stdout.write("\n\n");
+
+  // Find all students that have taken Physics
+  process.stdout.write("\n\nTesting queryStudents\n\n");
+  process.stdout.write("Students who have taken Physics\n");
+  await queryStudents("Physics");
+
+  // Find all students that have taken Computer Science
+  process.stdout.write("\n\nStudents who have taken Computer Science\n");
+  await queryStudents("Computer Science");
+
+  // Delete the students created in the first exercise
+  process.stdout.write("\n\nTesting deleteStudent\n\n");
+  let oldStudent = await getStudent("S101", "101");
+  if (oldStudent) {
+    await deleteStudent(oldStudent).then(() =>
+      getStudent(oldStudent.id, oldStudent.StudentNumber)
+    );
+  }
+
+  process.stdout.write("\n");
+
+  oldStudent = await getStudent("S102", "102");
+  if (oldStudent) {
+    await deleteStudent(oldStudent).then(() =>
+      getStudent(oldStudent.id, oldStudent.StudentNumber)
+    );
+  }
+
+  process.stdout.write("\n\nDone\n");
 }
 
 test();
